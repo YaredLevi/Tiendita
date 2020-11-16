05938a4600b0c96634e2632f6cb5f826a19a4d21
Yared Ordenes <yar.ordenes@alumnos.duoc.cl>
2020-11-16 01:16:22 -0300

    " modelo bd"

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,4 +1,4 @@
-"""config URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("blog.urls")),
-    path('accounts/', include("autenticacion.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('accounts/', include("autenticacion.urls")),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/blog/tests.py
+++
@@ -1,4 +0,0 @@
-from config.wsgi import *
-from blog.models import Categoria
-
-
